ea5633472956100cba4dd1c051c9bad23f142e44
ddongmin.kim <ddongmin.kim@navercorp.com>
2022-06-18 21:40:47 +0900

    add-youtube-modal

--- src/pages/YoutubePage.js
+++ src/pages/YoutubePage.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import YoutubeModal from '../components/YoutubeModal';
+import { Link, Navigate } from 'react-router-dom';
 
 const YoutubePage = () => {
-  return <div>YoutubePage</div>;
+  const currentIndex = 0;
+  const musics = [{ ID: 'tOtesnkPJeg', timestamp: 10 }];
+
+  if (musics === null) {
+    console.error('Fuck!');
+    return <Navigate to="/errorPage" />;
+  }
+
+  const isLast = currentIndex === musics.length - 1;
+  const music = musics[currentIndex];
+
+  return (
+    <div>
+      <YoutubeModal videoID={music.ID} timestamp={music.timestamp} />
+      <Link to={isLast ? '/finish' : '/question'}>Next</Link>
+    </div>
+  );
 };
 
 export default YoutubePage;
