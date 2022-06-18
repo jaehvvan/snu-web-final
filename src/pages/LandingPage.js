8c1b50e54de5bbc04c9e9c59d0b982899a540e56
kaheunlee <kaheunalicialee@gmail.com>
2022-06-19 01:30:39 +0900

    help

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/landingpage.css';
 
 const LandingPage = () => {
   return (
-    <div>
-      <div>신서유기 폰트는 과연 제대로 작동할까요?</div>
-      <Link to="/setting">시작</Link>
+    <div id="landing">
+      <div id="desc">
+        <div id="main_title">노래 제목 맞추기 게임</div>
+        <div id="main_title_desc">
+          "술자리에서든, MT에서든, {'\n'} 가벼운 즐거움이 필요한 곳 어디서든 플레이하세요"
+        </div>
+      </div>
+      <Link to="/setting" id="start_button">
+        시작
+      </Link>
+      <div id="team_name">made by 6조</div>
     </div>
   );
 };
