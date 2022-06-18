0378b4472c609b7aef4f878c3cdf8a83c1173574
probean <jhjung3@snu.ac.kr>
2022-06-18 22:21:05 +0900

    [fix] rebase 충돌 해결

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import './styles/reset.scss';
 import './styles/normalize.scss';
 import './styles/fonts.css';
+import './styles/scoreBoard.scss';
 
 import { BrowserRouter } from 'react-router-dom';
 
