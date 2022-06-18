3497bf153a28e5dfc4e30a8a14115c9e40a17b80
kaheunlee <kaheunalicialee@gmail.com>
2022-06-18 22:09:34 +0900

    Update LandingPage.js

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 const LandingPage = () => {
   return (
-    <body>
+    <div>
       <div>신서유기 폰트는 과연 제대로 작동할까요?</div>
       <Link to="/setting">시작</Link>
-    </body>
+    </div>
   );
 };
 
