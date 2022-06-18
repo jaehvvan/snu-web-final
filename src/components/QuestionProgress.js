8c2d07c382e787499e1c1b07e19ad00b0d6c8cf7
probean <jhjung3@snu.ac.kr>
2022-06-18 22:18:53 +0900

    [feature] QuestionPage 스켈레톤 생성 및 ScoreBoard 컴포넌트 개발

---
+++ src/components/QuestionProgress.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const QuestionProgress = () => {
+  return <div>QuestionProgress</div>;
+};
+
+export default QuestionProgress;
--- src/pages/QuestionPage.js
+++ src/pages/QuestionPage.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import QuestionProgress from '../components/QuestionProgress';
+import ScoreBoard from '../components/ScoreBoard';
+import MusicPlayer from '../components/MusicPlayer';
+import Hint from '../components/Hint';
+import { Link } from 'react-router-dom';
 
 const QuestionPage = () => {
-  return <div>QuestionPage</div>;
+  return (
+    <div>
+      <ScoreBoard />
+      <QuestionProgress />
+      <MusicPlayer />
+      <Hint />
+      <Link to="/youtube">정답 공개</Link>
+    </div>
+  );
 };
 
 export default QuestionPage;
