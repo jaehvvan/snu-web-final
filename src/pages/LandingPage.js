0c76d7762af22371bc9d355e15c6b558bb849d15
kaheunlee <kaheunalicialee@gmail.com>
2022-06-18 22:06:53 +0900

    new font yay

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,9 +3,10 @@ import { Link } from 'react-router-dom';
 
 const LandingPage = () => {
   return (
-    <div>
+    <body>
+      <div>신서유기 폰트는 과연 제대로 작동할까요?</div>
       <Link to="/setting">시작</Link>
-    </div>
+    </body>
   );
 };
 
