a9e339e0c4b6c3591911d4fc2dc25ac3c30bf11e
probean <jhjung3@snu.ac.kr>
2022-06-18 20:59:40 +0900

    [feature] 스켈레톤 생성 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
+
+import FinishPage from './pages/FinishPage';
+import LandingPage from './pages/LandingPage';
+import QuestionPage from './pages/QuestionPage';
+import SettingPage from './pages/SettingPage';
+import YoutubePage from './pages/YoutubePage';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/setting" element={<SettingPage />} />
+      <Route path="/question" element={<QuestionPage />} />
+      <Route path="/youtube" element={<YoutubePage />} />
+      <Route path="/finish" element={<FinishPage />} />
+    </Routes>
+  );
 }
 
 export default App;
